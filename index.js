ffd7ffabce7b1cc782390048b43a5f9c695f361f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-06-06 11:14:46 -0400

    feat: data post to db method added

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+
+    app.post('/coffee',async(req,res)=>{
+    	const newCoffee = req.body;
+    	console.log(newCoffee)
+    	const result= await coffeeCollection.insertOne(newCoffee);
+    	res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,9 +53,6 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
 	res.send('server is running !')
 })
-
-//LuB82XOG0BhoKKax
-
 
 
 app.listen(port);
